af9544ee1e76a34af82636b9eb4e118b325d4ea8
Brian Albert <brian.david.albert@gmail.com>
2023-01-01 13:21:30 -0500

    added react router-dom; added component folders and files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+//page components
+import Home from './pages/home/Home'
+import About from './pages/about/AboutUs'
+import Contact from './pages/contact/Contact'
+import Login from './pages/login/Login'
+import Projects from './pages/projects/OurProjects'
+
+
+//styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/about/AboutUs.js
@@ -0,0 +1,10 @@
+//styles
+import './aboutUs.css'
+
+
+
+export default function AboutUs() {
+  return (
+    <div>About Us</div>
+  )
+}
